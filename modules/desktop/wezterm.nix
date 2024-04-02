dd6ca33f7c2866af918903216038b461c1f6b707
Oliver Sargison <sargo@sargo.cc>
2024-04-02 20:01:23 +1300

    SYSTEM REBUILD Tue, 2 Apr 2024 20:01:23 +1300 (now)

--- modules/desktop/wezterm.nix
+++ modules/desktop/wezterm.nix
@@ -2,6 +2,11 @@
   home-manager.sharedModules = [
     ({pkgs, ...}: {
       programs.wezterm.enable = true;
+      programs.wezterm.extraConfig =
+        #lua
+        ''
+          config.font = wezterm.font 'JetbrainsMono Nerd Font'
+        '';
     })
   ];
 }
