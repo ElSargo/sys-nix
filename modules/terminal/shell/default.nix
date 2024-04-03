a65326dac17dfa6ec8157f2925c7969ce1901648
Oliver Sargison <sargo@sargo.cc>
2024-04-04 08:23:21 +1300

    SYSTEM REBUILD Thu, 4 Apr 2024 08:23:21 +1300 (now)

--- modules/terminal/shell/default.nix
+++ modules/terminal/shell/default.nix
@@ -25,6 +25,7 @@
       ];
 
       programs.carapace.enable = true;
+      programs.carapace.programs.carapace.enableNushellIntegration = false;
       programs.nushell = {
         package = pkgs.nushellFull; # Nu shell 0.91 is broken with carapace
         enable = true;
@@ -47,6 +48,46 @@
               }
             }
 
+            let fish_completer = {|spans|
+                fish --command $'complete "--do-complete=($spans | str join " ")"'
+                | $"value(char tab)description(char newline)" + $in
+                | from tsv --flexible --no-infer
+            }
+
+            let carapace_completer = {|spans: list<string>|
+                carapace $spans.0 nushell ...$spans
+                | from json
+                | if ($in | default [] | where value =~ '^-.*ERR$' | is-empty) { $in } else { null }
+            }
+
+            # This completer will use carapace by default
+            let external_completer = {|spans|
+                let expanded_alias = scope aliases
+                | where name == $spans.0
+                | get -i 0.expansion
+
+                let spans = if $expanded_alias != null {
+                    $spans
+                    | skip 1
+                    | prepend ($expanded_alias | split row ' ' | take 1)
+                } else {
+                    $spans
+                }
+
+                match $spans.0 {
+                    # carapace completions are incorrect for nu
+                    nu => $fish_completer
+                    # fish completes commits and branch names in a nicer way
+                    git => $fish_completer
+                    # carapace doesn't have completions for asdf
+                    asdf => $fish_completer
+                    # use zoxide completions for zoxide commands
+                    __zoxide_z | __zoxide_zi => $zoxide_completer
+                    _ => $carapace_completer
+                } | do $in $spans
+            }
+
+
             $env.config = {
               table: {
                 mode: rounded
@@ -58,6 +99,12 @@
               }
               rm: {
                 always_trash: true
+              }
+              completions: {
+                  external: {
+                      enable: true
+                      completer: $external_completer
+                  }
               }
               hooks: {
                   env_change: {
