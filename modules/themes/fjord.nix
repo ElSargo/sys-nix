a6965ed80e003e74a2804bc6b32253fe9532bde1
Oliver Sargison <sargo@sargo.cc>
2024-04-12 12:27:24 +1200

    SYSTEM REBUILD Fri, 12 Apr 2024 12:27:24 +1200 (now)

--- modules/themes/fjord.nix
+++ modules/themes/fjord.nix
@@ -1,8 +1,8 @@
 {pkgs, ...}: {
   theme = null;
   wallpaper = pkgs.fetchurl {
-    url = "https://unsplash.com/photos/uOi3lg8fGl4/download?ixid=M3wxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNzEyODc1NDc2fA&force=true";
-    sha256 = "sha256-mzHUmGCY+/Crz9+YZfTLGgVKl8nr6sAypoCHEEugL68=";
+    url = "https://unsplash.com/photos/QHTch5kP2qY/download?ixid=M3wxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNzEyODgwNzg4fA&force=true";
+    sha256 = "sha256-zl4VoyRk/5R5fa6B7aJRbMqa6qml9q0EzdmzvkYF1e0=";
   };
 
   stylix.polarity = "dark";
