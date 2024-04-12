948b145fe53d7bb69ed34ea7b5557046cc59f82c
Oliver Sargison <sargo@sargo.cc>
2024-04-12 15:39:40 +1200

    SYSTEM REBUILD Fri, 12 Apr 2024 15:39:40 +1200 (now)

--- modules/themes/exorsist.nix
+++ modules/themes/exorsist.nix
@@ -1,5 +1,5 @@
 {pkgs, ...}: let
-  repo = pkgs.fetchGit {
+  repo = pkgs.fetchgit {
     url = "https://codeberg.org/exorcist/wallpapers";
     rev = "7f2d29df30958703bc3c3fe1e20eb2e5a0ccaf22";
   };
