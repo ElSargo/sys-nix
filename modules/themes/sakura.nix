84b7c8986fd4993274ffaa4d019aef784672043c
Oliver Sargison <sargo@sargo.cc>
2024-04-12 16:08:46 +1200

    SYSTEM REBUILD Fri, 12 Apr 2024 16:08:46 +1200 (now)

--- modules/themes/sakura.nix
+++ modules/themes/sakura.nix
@@ -1,8 +1,8 @@
 {pkgs, ...}: {
   theme = null;
   wallpaper = pkgs.fetchurl {
-    url = "https://unsplash.com/photos/uOi3lg8fGl4/download?ixid=M3wxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNzEyODc1NDc2fA&force=true";
-    sha256 = "sha256-mzHUmGCY+/Crz9+YZfTLGgVKl8nr6sAypoCHEEugL68=";
+    url = "https://github.com/iluvgirlswithglasses/dotfiles/blob/9c1c911a59925e75b6144b695f3d4249c4e11c21/.wallpapers/sakura--rchannel-boosted--1920-1080.png";
+    sha256 = "sha256-u7Z0FIIb0I1JQ0txhEfFUWb5fz1VA6GO7gNiPDhsXJI=";
   };
 
   stylix.polarity = "dark";
