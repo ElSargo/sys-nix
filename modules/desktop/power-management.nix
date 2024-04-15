c90fa699b5dad547a13c43b79ddfd2e96aa14a08
Oliver Sargison <sargo@sargo.cc>
2024-04-16 10:03:34 +1200

    SYSTEM REBUILD Tue, 16 Apr 2024 10:03:34 +1200 (now)

--- modules/desktop/power-management.nix
+++ modules/desktop/power-management.nix
@@ -24,25 +24,25 @@
       };
     };
     tlp = {
-      # enable = true;
-      settings = {
-        CPU_DRIVER_OPMODE_ON_AC = ""; # "active";
-        CPU_DRIVER_OPMODE_ON_BAT = ""; # "active";
-        CPU_SCALING_GOVERNOR_ON_AC = ""; # "powersave";
-        CPU_SCALING_GOVERNOR_ON_BAT = ""; # "powersave";
-        CPU_SCALING_MIN_FREQ_ON_AC = ""; # 0;
-        CPU_SCALING_MAX_FREQ_ON_AC = ""; # 9999999;
-        CPU_SCALING_MIN_FREQ_ON_BAT = ""; # 0;
-        CPU_SCALING_MAX_FREQ_ON_BAT = ""; # 9999999;
-        CPU_ENERGY_PERF_POLICY_ON_AC = ""; # "balance_performance";
-        CPU_ENERGY_PERF_POLICY_ON_BAT = ""; # "balance_power";
-        CPU_MIN_PERF_ON_AC = ""; # 0;
-        CPU_MAX_PERF_ON_AC = ""; # 100;
-        CPU_MIN_PERF_ON_BAT = ""; # 0;
-        CPU_MAX_PERF_ON_BAT = ""; # 30;
-        CPU_BOOST_ON_AC = 1;
-        CPU_HWP_DYN_BOOST_ON_AC = 1;
-      };
+      enable = true;
+      # settings = {
+      #   CPU_DRIVER_OPMODE_ON_AC = ""; # "active";
+      #   CPU_DRIVER_OPMODE_ON_BAT = ""; # "active";
+      #   CPU_SCALING_GOVERNOR_ON_AC = ""; # "powersave";
+      #   CPU_SCALING_GOVERNOR_ON_BAT = ""; # "powersave";
+      #   CPU_SCALING_MIN_FREQ_ON_AC = ""; # 0;
+      #   CPU_SCALING_MAX_FREQ_ON_AC = ""; # 9999999;
+      #   CPU_SCALING_MIN_FREQ_ON_BAT = ""; # 0;
+      #   CPU_SCALING_MAX_FREQ_ON_BAT = ""; # 9999999;
+      #   CPU_ENERGY_PERF_POLICY_ON_AC = ""; # "balance_performance";
+      #   CPU_ENERGY_PERF_POLICY_ON_BAT = ""; # "balance_power";
+      #   CPU_MIN_PERF_ON_AC = ""; # 0;
+      #   CPU_MAX_PERF_ON_AC = ""; # 100;
+      #   CPU_MIN_PERF_ON_BAT = ""; # 0;
+      #   CPU_MAX_PERF_ON_BAT = ""; # 30;
+      #   CPU_BOOST_ON_AC = 1;
+      #   CPU_HWP_DYN_BOOST_ON_AC = 1;
+      # };
     };
     power-profiles-daemon.enable = false;
   };
