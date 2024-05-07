232e1a131e3c92fbab554bd12ffd5de3fbb47f83
Oliver Sargison <sargo@sargo.cc>
2024-05-08 08:14:43 +1200

    SYSTEM REBUILD Wed, 8 May 2024 08:14:43 +1200 (now)

---
+++ modules/themes/teran.nix
@@ -0,0 +1,9 @@
+{pkgs, ...}: {
+  theme = null;
+  wallpaper = pkgs.fetchurl {
+    url = "https://unsplash.com/photos/ii5JY_46xH0/download?force=true";
+    sha256 = "sha256-fg7K/pnqePOj4XDAb1RQremx2FQF4fs1TSBOHQFqVXs=";
+  };
+
+  stylix.polarity = "light";
+}
